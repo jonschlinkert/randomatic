ff5fdf9195b9188564eb76419bc1e9c0efcf8585
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-02 11:25:47 -0500

    update copyright, root files

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /**
  * randomatic <https://github.com/jonschlinkert/randomatic>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
